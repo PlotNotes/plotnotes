c0fc8e8f248d4c14c822250d55811b34ea764db1
noah <noah.edward.campbell@gmail.com>
2023-05-03 16:07:54 -0600

    Improved UI
    Changed logo to more fit the theme and style of the surrounding color and website, as well as adding a login button that redirects the user to a login subdomain that currently does not exist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   // Returns the home page of PlotNotes with a welcome message and displaying the logo above it
-  // The logo is inside of a folder called images inside the public folder  
+  // Adds a login button that redirects to the login page, located on the top right of the page
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -15,10 +15,10 @@ export default function Home() {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <Image
-        src="/images/PlotNotes.png"
+        src="/images/PlotNotesLogo.png"
         alt="PlotNotes Logo"
-        width={200}
-        height={200}
+        width={600}
+        height={600}
       />
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="font-inter text-6xl font-bold">
@@ -27,6 +27,9 @@ export default function Home() {
         <p className="font-inter text-2xl mt-3">
           A place to write your stories and share them with the world
         </p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10">
+          <a href="/login">Login</a>
+        </button>
       </main>
     </div>
   )
