594fd7a67162ffff912da4ea33ce71e504338bf9
noah <noah.edward.campbell@gmail.com>
2023-05-11 13:12:08 -0700

    prevent duplicate users from being stored

--- src/pages/api/sessionCmds.ts
+++ src/pages/api/sessionCmds.ts
@@ -3,51 +3,73 @@ import { query } from './db';
 import Cookies from 'cookies';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { username, password, usedGoogle } = req.body;
+    const { username, password, usedGoogle, email } = req.body;
     try {
         if (usedGoogle) {
-            const result = await query(
-                `INSERT INTO users (name, usedGoogle) VALUES ($1, $2);`,
-                [username, usedGoogle]
-            );
+            const sessionId = await signInWithGoogle(username, email);
 
-            const sessionId = await createSession(username);
             const cookie = new Cookies(req, res);
 
             cookie.set('token', sessionId, {
                 httpOnly: true,
-              });
+            });
             res.status(200).send({ sessionId: sessionId});
         }
         else {
-            const addUser = await query(
-                `INSERT INTO users (name, usedGoogle) VALUES ($1, $2);`,
-                [username, usedGoogle]
+            // Checks to see if a user with the same email already exists
+            const emailQuery = await query(
+                `SELECT * FROM users WHERE email = $1`,
+                [email]
             );
 
-            const idQuery = await query(
-                `SELECT (id) FROM users WHERE name = $1`,
-                [username]
-            );
-
-            const id = idQuery.rows[0].id;
-            const addPassword = await query(
-                `INSERT INTO userpasswords (id, password) VALUES ($1, $2);`,
-                [id, password]
-            );
-
-            const sessionId = await createSession(username);
+            // If a user with the same email already exists, inform the user
+            if (emailQuery.rows.length > 0) {
+                res.status(200).send({ error: 'A user with the same email already exists' });
+                return;
+            }
+            const sessionId = await signIn(username, password, email);
             const cookie = new Cookies(req, res);
             cookie.set('token', sessionId, {
                 httpOnly: true,
                 });
 
-            res.status(200).send(addUser.rows[0]);
+            res.status(200).send(sessionId);
         }
     } catch (err) {
         console.error(err);
         res.status(500).send({ error: err + ' error in queries' });
     }
+}
+
+async function signIn(username: string, password: string, email: string): Promise<string> {
+
+    const addUser = await query(
+        `INSERT INTO users (name, usedGoogle, email) VALUES ($1, $2, $3);`,
+        [username, false, email]
+    );
+
+    const idQuery = await query(
+        `SELECT (id) FROM users WHERE name = $1`,
+        [username]
+    );
+
+    const id = idQuery.rows[0].id;
+    const addPassword = await query(
+        `INSERT INTO userpasswords (id, password) VALUES ($1, $2);`,
+        [id, password]
+    );
+
+    return createSession(username);
+}
+
+async function signInWithGoogle(username: string, email: string): Promise<string> {
+    const user = await query(
+        `INSERT INTO users (name, usedGoogle, email) VALUES ($1, $2, $3);`,
+        [username, true, username]
+    );
+
+    const sessionId = await createSession(username);
+    return sessionId;
 }
 
 export async function createSession(username: string): Promise<string> {
